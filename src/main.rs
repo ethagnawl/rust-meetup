85caba08269a96b971f6428157ec0c6f0b368a09
Peter Doherty <pdoherty926@gmail.com>
2018-03-28 20:14:35 -0400

    update dependencies; first pass at making mta http request

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+extern crate futures;
+extern crate hyper;
+extern crate tokio_core;
+
+use std::io::{self, Write};
+use futures::{Future, Stream};
+use hyper::Client;
+use tokio_core::reactor::Core;
+
 fn main() {
-    println!("Hello, world!");
+    let mut core = Core::new()?;
+    let client = Client::new(&core.handle());
+    let uri = "http://web.mta.info/status/serviceStatus.txt".parse()?;
+    let work = client.get(uri).map(|res| {
+        println!("Response: {}", res.status());
+    });
 }
